87eafa8afba583feda54107f06cc84678ad9503b
Avineet Bhat <surajbhat316@gmail.com>
2020-05-19 21:49:05 +0530

    Add files via upload

---
+++ gmane/gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','indiana.edu','berkeley.edu','mac.com','cam.ac.uk','uct.ac.za','ucdavis.edu','gmail.com','yale.edu','ufp.pt'],
+['2005-',56,12,12,12,7,14,11,10,5,10],
+['2006-',473,168,164,161,165,112,112,102,100,89]
+];
